a673a5a35eb7c5c9b5743548085d76f520d5f1ba
jchenek <105424893+jchenek@users.noreply.github.com>
2024-04-08 12:00:44 +0800

    Add files via upload

--- one-step-DB-download-p1-general.sh
+++ one-step-DB-download-p1-general.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #sh one-step-DB-download.sh <IN_path_to_OSDB>
+#if download fail, directly rerun the scripts
 echo "Path to OSDB: $1"
 echo "Path to OSDB: $1" >> OSDB_Download.log
 echo "OSDB pipeline p1 start at:" >> OSDB_Download.log
@@ -10,7 +11,7 @@ echo "cazy start at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) >> OSDB_Download.log
 mkdir cazy
 cd cazy
-aria2c -i $1/DB_links/link_cazy.txt
+aria2c -c -i $1/DB_links/link_cazy.txt
 cd ../
 echo "cazy finish at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) "\n" >> OSDB_Download.log
@@ -20,7 +21,7 @@ echo "cog start at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) >> OSDB_Download.log
 mkdir cog
 cd cog
-aria2c -i $1/DB_links/link_cog.txt
+aria2c -c -i $1/DB_links/link_cog.txt
 cd ../
 echo "cog finish at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) "\n" >> OSDB_Download.log
@@ -30,7 +31,7 @@ echo "tcdb start at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) >> OSDB_Download.log
 mkdir tcdb
 cd tcdb
-aria2c -i $1/DB_links/link_tcdb.txt
+aria2c -c -i $1/DB_links/link_tcdb.txt
 cd ../
 echo "tcdb finish at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) "\n" >> OSDB_Download.log
@@ -40,7 +41,7 @@ echo "merops start at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) >> OSDB_Download.log
 mkdir merops
 cd merops
-aria2c -i $1/DB_links/link_merops.txt
+aria2c -c -i $1/DB_links/link_merops.txt
 cd ../
 echo "merops finish at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) "\n" >> OSDB_Download.log
@@ -50,7 +51,7 @@ echo "swissprot start at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) >> OSDB_Download.log
 mkdir swissprot
 cd swissprot
-aria2c -i $1/DB_links/link_swissprot.txt
+aria2c -c -i $1/DB_links/link_swissprot.txt
 cd ../
 echo "swissprot finish at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) "\n" >> OSDB_Download.log
@@ -60,7 +61,7 @@ echo "pfam-A start at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) >> OSDB_Download.log
 mkdir pfam-A
 cd pfam-A
-aria2c -i $1/DB_links/link_pfam-A.txt
+aria2c -c -i $1/DB_links/link_pfam-A.txt
 cd ../
 echo "pfam-A finish at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) "\n" >> OSDB_Download.log
@@ -70,7 +71,7 @@ echo "UniProtKB-TrEMBL start at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) >> OSDB_Download.log
 mkdir UniProtKB-TrEMBL
 cd UniProtKB-TrEMBL
-aria2c -i $1/DB_links/link_UniProtKB-TrEMBL.txt
+aria2c -c -i $1/DB_links/link_UniProtKB-TrEMBL.txt
 cd ../
 echo "UniProtKB-TrEMBL finish at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) "\n" >> OSDB_Download.log
--- one-step-DB-download-p2-nr.sh
+++ one-step-DB-download-p2-nr.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 #sh one-step-DB-download.sh <IN_path_to_OSDB>
+#if download fail, directly rerun the scripts
 
 ########nr
 echo "nr start at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) >> OSDB_Download.log
 mkdir nr
 cd nr
-aria2c -i $1/DB_links/link_nr.txt
+aria2c -c -i $1/DB_links/link_nr.txt
 cd ../
 echo "nr finish at:" >> OSDB_Download.log
 echo $(date +%Y-%m-%d" "%H:%M:%S) "\n" >> OSDB_Download.log
